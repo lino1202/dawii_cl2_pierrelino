b3d2c33b12fa379e88a79d572283b7fec788f550
lino1202 <pier-lino@hotmail.com>
2023-05-29 16:04:23 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/dawii_cl2_pierrelino/DawiiCl2PierrelinoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dawii_cl2_pierrelino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2PierrelinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2PierrelinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/dawii_cl2_pierrelino/DawiiCl2PierrelinoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dawii_cl2_pierrelino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2PierrelinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
